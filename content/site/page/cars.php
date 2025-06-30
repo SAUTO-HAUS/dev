f137e24b921b06f870c7d470ba4f30538ed4a749
GrigoreBotnarenco <botnarenco1996@mail.com>
2025-06-30 22:11:39 +0300

    go direct table name

--- content/site/page/cars.php
+++ content/site/page/cars.php
@@ -312,7 +312,7 @@ elseif (is_numeric($t_mp[3]) || (isset($t_mp[3]) && !is_numeric($t_mp[3]) && !is
 			$spec_ar = ['yr', 'bt', 'mlg', 'vol', 'hp', 'fl', 'tra', 'wd', 'clr', 'sts', 'loc', 'import_country_id'];
 			
             //Update views
-            $pdo2 = $db->prepare('UPDATE '.$prefx.'_car_ctlg SET `views` = `views` + 1 WHERE `id`='.$it_id);
+            $pdo2 = $db->prepare('UPDATE gh3sp_car_ctlg SET `views` = `views` + 1 WHERE `id`='.$it_id);
             $pdo2->execute();
 
 			$pdo = $db->prepare('SELECT * FROM '.$prefx.'_car_ctlg WHERE `id`= :id AND `vis`="1" AND `act`="1" LIMIT 1');
