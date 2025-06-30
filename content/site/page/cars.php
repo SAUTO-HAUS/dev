cc75cbce81fc8b6948dd521e5edd79953def529d
GrigoreBotnarenco <botnarenco1996@mail.com>
2025-06-30 22:34:59 +0300

    query insteadof prepare

--- content/site/page/cars.php
+++ content/site/page/cars.php
@@ -312,8 +312,7 @@ elseif (is_numeric($t_mp[3]) || (isset($t_mp[3]) && !is_numeric($t_mp[3]) && !is
 			$spec_ar = ['yr', 'bt', 'mlg', 'vol', 'hp', 'fl', 'tra', 'wd', 'clr', 'sts', 'loc', 'import_country_id'];
 			
             //Update views
-            $pdo2 = $db->prepare('UPDATE gh3sp_car_ctlg SET `views` = `views` + 1 WHERE `id`='.$it_id);
-            $pdo2->execute();
+            $db->query('UPDATE '.$prefx.'_car_ctlg SET `views` = `views` + 1 WHERE `id` = '.$it_id);
 
 			$pdo = $db->prepare('SELECT * FROM '.$prefx.'_car_ctlg WHERE `id`= :id AND `vis`="1" AND `act`="1" LIMIT 1');
 			$pdo->execute(['id' => $it_id]);
