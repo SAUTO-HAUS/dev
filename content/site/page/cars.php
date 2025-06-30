7676a3deb3df2150076bd3e0e24a1a74cab5b14c
GrigoreBotnarenco <botnarenco1996@mail.com>
2025-06-30 22:09:19 +0300

    hard code the id

--- content/site/page/cars.php
+++ content/site/page/cars.php
@@ -313,7 +313,7 @@ elseif (is_numeric($t_mp[3]) || (isset($t_mp[3]) && !is_numeric($t_mp[3]) && !is
 			
             //Update views
             $pdo2 = $db->prepare('UPDATE '.$prefx.'_car_ctlg SET `views` = 10 WHERE `id`=:id');
-            $pdo2->execute(['id' => $it_id]);
+            $pdo2->execute(['id' => 11638]);
 
 			$pdo = $db->prepare('SELECT * FROM '.$prefx.'_car_ctlg WHERE `id`= :id AND `vis`="1" AND `act`="1" LIMIT 1');
 			$pdo->execute(['id' => $it_id]);
