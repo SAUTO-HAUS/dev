0c6276a52cb9739c9e5826de8974da7038ea22fb
GrigoreBotnarenco <botnarenco1996@mail.com>
2025-07-31 20:25:57 +0300

    update cars.php

--- content/site/page/cars.php
+++ content/site/page/cars.php
@@ -312,7 +312,7 @@ elseif (is_numeric($t_mp[3]) || (isset($t_mp[3]) && !is_numeric($t_mp[3]) && !is
 			$spec_ar = ['yr', 'bt', 'mlg', 'vol', 'hp', 'fl', 'tra', 'wd', 'clr', 'sts', 'loc', 'import_country_id'];
 			
             //Update views
-            // $db->query('UPDATE '.$prefx.'_car_ctlg SET `views` = `views` + 1 WHERE `id` = '.$it_id);
+            $db->query('UPDATE '.$prefx.'_car_ctlg SET `views` = `views` + 1 WHERE `id` = '.$it_id);
 
 			$pdo = $db->prepare('SELECT * FROM '.$prefx.'_car_ctlg WHERE `id`= :id AND `vis`="1" AND `act`="1" LIMIT 1');
 			$pdo->execute(['id' => $it_id]);
