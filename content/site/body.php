0ed9d73836d7a03c7c949e01caad31a7df6c8575
GrigoreBotnarenco <botnarenco1996@mail.com>
2025-09-05 11:34:19 +0300

    update cookie 3

--- content/site/body.php
+++ content/site/body.php
@@ -34,7 +34,7 @@ include(_SITE_INCL.'/functions.php'); ?>
 	
 	<div id="cons_bx" class="cons_bx" style="display:none;">
 		<div class="close-banner" onclick="setConsent(true, false, false, false);">×</div>
-		<div style="max-width:50%; padding:0 1rem 0 0;">
+		<div>
 			<p><?php echo $lng['l']['consent']['base_txt'][0].'<span style="border-bottom:1px solid #e2001a;">'.$lng['l']['consent']['acpt_all'].'</span>'.$lng['l']['consent']['base_txt'][1].' <a href="/'.$_COOKIE['lang'].'/privacy" target="_blank" style="color:#000;">"'.$lng['l']['menu']['privacy'].'"</a>'; ?></p>
 		</div>
 		<div class="cons_btns">
