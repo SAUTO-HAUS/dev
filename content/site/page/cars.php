4a92280cb75987a68c1682f21090e4b78adb1c33
Serghei <support@zhost.md>
2025-09-19 13:35:58 +0300

    params html 2

--- content/site/page/cars.php
+++ content/site/page/cars.php
@@ -734,6 +734,8 @@ elseif (is_numeric($t_mp[3]) || (isset($t_mp[3]) && !is_numeric($t_mp[3]) && !is
                                 '.$o_prc_bl.'
                             </div>
                             <div class="doit">
+                                <div class="btn msg" onclick=" openParamsPopAuto()" > '.$lng['w']['characteristics'].' </div>
+                                
                                 <a class="btn call" href="tel:'.$dynamicPhone.'">'.$lng['w']['call'].'</a>
                                 
                                  <div class="btn msg2">
