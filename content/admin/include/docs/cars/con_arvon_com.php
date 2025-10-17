e94364823a238f0d8510b883f7f124cba6630684
GrigoreBotnarenco <botnarenco1996@mail.com>
2025-10-17 21:24:52 +0300

    con_arvon_com.php

--- content/admin/include/docs/cars/con_arvon_com.php
+++ content/admin/include/docs/cars/con_arvon_com.php
@@ -6,7 +6,7 @@ $rtrn = '
 <style>
 	.logo {float:left;}
 	.date {text-align:center; float:left;}
-	.ttl {text-align:center; padding:15mm 0 0; clear:both; font-size:1.4rem; font-weight:bold;}
+	.ttl {text-align:center; padding:5mm 0 0; clear:both; font-size:1.4rem; font-weight:bold;}
 	tr > td.id {width:10%;} tr > td.nm {width:50%;} tr > td.prc {width:40%;}
 	.cont {margin:0;}
 	.txt_up {text-transform:uppercase;}
@@ -93,6 +93,18 @@ $rtrn = '
 			<p>Cu condiția ca clauzele prezentului contract au fost respectate de cumpărător și tranzacția nu a avut loc din vina vânzătorului, sau vânzătorul a refuzat/renunțat să contracteze, sau licitația nu a fost favorabilă, atunci arvuna urmează a fi restituită integral în valoare de 100%.</p>
 		</div>
 		
+         <div style="text-align: center;">
+					<span style="font-weight: bold; font-size: 0.8rem;">Declarații ale clientului:</span>
+			</div>
+			<div style="text-align: justify; margin-bottom: 0.5mm; line-height: 3.5mm;">
+					<span style="font-size: 0.75rem;"><span style="font-weight: bold;">1.</span> Confirm corectitudinea datelor prezentate și îmi asum obligația să comunic în scris către SAUTO SRL orice modificare referitoare la cele declarate mai sus, în termen de 5 zile lucrătoare de la data eliberării documentului confirmativ. Confirm, de asemenea, proveniența legală a mijloacelor bănești, încasate/depuse în cont, inclusiv a mijloacelor ce vor derula prin contul/conturile mele.<br/>
+					<span style="font-weight: bold;">2.</span> Îmi exprim consimțământul expres la prelucrarea de către SAUTO SRL a datelor mele cu caracter personal care sunt prelucrate în scopuri legate de deservirea calitativă aferentă serviciilor prestate de SAUTO SRL, inclusiv alte cazuri conform legislației în vigoare. Aceste acțiuni pot fi efectuate și prin utilizarea mijloacelor informatice automatizate în conformitate cu prevederile Legii Nr. 133 din 08.07.2011 privind protecția datelor cu caracter personal.<br/>
+					<span style="font-weight: bold;">3.</span> Prin prezentul, declar pe propria răspundere că sunt beneficiarul efectiv al contului/lor, implicit al mijloacelor bănești utilizate prin intermediul acestuia/acestora.</span>
+			</div>
+		<div style="margin-top: 1mm; font-size: 0.7rem; text-align: justify;">
+				<span style="vertical-align: super; font-size: 0.65rem;">1</span> <span style="font-weight: 600;">Persoană expusă politic</span> - persoana fizică definită conform <span style="font-weight: 600;">art. 8 din Legea nr.308/2017</span> cu privire la prevenirea și combaterea spălării banilor și finanțării terorismului.
+		</div>
+
 		<div class="flx">
 			<div class="ws"></div>
 			<table class="n2">
