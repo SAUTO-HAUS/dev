b5bc3d3dd60e6aecdc2f7910b85d994ada2eba8e
GrigoreBotnarenco <botnarenco1996@mail.com>
2025-10-17 21:35:01 +0300

    vinzare_avans.php

--- content/admin/include/docs/cars/vinzare_avans.php
+++ content/admin/include/docs/cars/vinzare_avans.php
@@ -248,6 +248,18 @@ $rtrn = '
 					</div>
 				</div>
 			</div>
+
+			<div style="text-align: center;">
+					<span style="font-weight: bold; font-size: 0.8rem;">Declarații ale clientului:</span>
+			</div>
+			<div style="text-align: justify; margin-bottom: 0.5mm; line-height: 3.5mm;">
+					<span style="font-size: 0.75rem;"><span style="font-weight: bold;">1.</span> Confirm corectitudinea datelor prezentate și îmi asum obligația să comunic în scris către SAUTO SRL orice modificare referitoare la cele declarate mai sus, în termen de 5 zile lucrătoare de la data eliberării documentului confirmativ. Confirm, de asemenea, proveniența legală a mijloacelor bănești, încasate/depuse în cont, inclusiv a mijloacelor ce vor derula prin contul/conturile mele.<br/>
+					<span style="font-weight: bold;">2.</span> Îmi exprim consimțământul expres la prelucrarea de către SAUTO SRL a datelor mele cu caracter personal care sunt prelucrate în scopuri legate de deservirea calitativă aferentă serviciilor prestate de SAUTO SRL, inclusiv alte cazuri conform legislației în vigoare. Aceste acțiuni pot fi efectuate și prin utilizarea mijloacelor informatice automatizate în conformitate cu prevederile Legii Nr. 133 din 08.07.2011 privind protecția datelor cu caracter personal.<br/>
+					<span style="font-weight: bold;">3.</span> Prin prezentul, declar pe propria răspundere că sunt beneficiarul efectiv al contului/lor, implicit al mijloacelor bănești utilizate prin intermediul acestuia/acestora.</span>
+			</div>
+		<div style="margin-top: 1mm; margin-bottom: 2mm;font-size: 0.7rem; text-align: justify;">
+				<span style="vertical-align: super; font-size: 0.65rem;">1</span> <span style="font-weight: 600;">Persoană expusă politic</span> - persoana fizică definită conform <span style="font-weight: 600;">art. 8 din Legea nr.308/2017</span> cu privire la prevenirea și combaterea spălării banilor și finanțării terorismului.
+		</div>
 			<div class="ws"></div>
 			<div class="res">
 				<b>Din ce surse ati aflat de produsul nostru:</b><br/>
