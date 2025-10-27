d24fdce66b4ec9cf6a29a79384da3dbbe1afdc80
GrigoreBotnarenco <botnarenco1996@mail.com>
2025-10-27 20:13:13 +0200

    update page public admin

--- App/Services/PublicationSettings.php
+++ App/Services/PublicationSettings.php
@@ -25,16 +25,16 @@ $lng['ro']['w']['publication_settings_title'] = 'Setări publicare automobile';
 $lng['ro']['w']['publication_settings_saved'] = 'Setările de publicare au fost salvate!';
 $lng['ro']['w']['cars_in_stock'] = 'Automobile în stoc';
 $lng['ro']['w']['cars_on_order'] = 'Automobile sub comandă';
-$lng['ro']['w']['api_999md_main'] = 'API "Три Девятки МД" - Cont principal';
-$lng['ro']['w']['api_999md_separate'] = 'API "Три Девятки МД" - Cont separat';
+$lng['ro']['w']['api_999md_main'] = 'API "999.md" - Cont principal';
+$lng['ro']['w']['api_999md_separate'] = 'API "999.md" - Cont separat';
 $lng['ro']['w']['account_login'] = 'Cont/Login:';
 $lng['ro']['w']['api_token'] = 'Token API:';
-$lng['ro']['w']['telegram_channel_1'] = 'Telegram - Canalul №1';
-$lng['ro']['w']['telegram_separate'] = 'Telegram - Canal separat';
-$lng['ro']['w']['bot_token'] = 'Bot Token:';
+$lng['ro']['w']['telegram_channel_1'] = 'Telegram';
+$lng['ro']['w']['telegram_separate'] = 'Telegram';
+$lng['ro']['w']['bot_token'] = 'Token Bot:';
 $lng['ro']['w']['chat_id'] = 'Chat ID:';
-$lng['ro']['w']['facebook_main'] = 'Facebook - Pagina principală';
-$lng['ro']['w']['facebook_same'] = 'Facebook - Aceeași pagină cu eticheta "Под заказ"';
+$lng['ro']['w']['facebook_main'] = 'Facebook';
+$lng['ro']['w']['facebook_same'] = 'Facebook';
 $lng['ro']['w']['page_id'] = 'Page ID:';
 $lng['ro']['w']['page_token'] = 'Page Token:';
 $lng['ro']['w']['auto_publish_stock'] = 'Publicare automată la adăugarea automobilului în stoc';
@@ -43,21 +43,35 @@ $lng['ro']['w']['save_settings'] = 'Salvează setările';
 $lng['ro']['w']['separate_account'] = 'Cont separat pentru comenzi';
 $lng['ro']['w']['same_page_id'] = 'ID pagină Facebook pentru comenzi';
 
+// Placeholder translations - Romanian
+$lng['ro']['w']['placeholder_main_999md_account'] = 'Contul principal 999.md';
+$lng['ro']['w']['placeholder_main_999md_token'] = 'Token API pentru contul principal';
+$lng['ro']['w']['placeholder_main_telegram_token'] = 'Token bot pentru canalul principal';
+$lng['ro']['w']['placeholder_main_telegram_chat'] = 'Chat ID canalul principal';
+$lng['ro']['w']['placeholder_main_facebook_page'] = 'ID pagina Facebook principală';
+$lng['ro']['w']['placeholder_main_facebook_token'] = 'Token acces pentru pagina principală';
+$lng['ro']['w']['placeholder_order_999md_account'] = 'Cont separat pentru comenzi';
+$lng['ro']['w']['placeholder_order_999md_token'] = 'Token API pentru contul de comenzi';
+$lng['ro']['w']['placeholder_order_telegram_token'] = 'Token bot pentru canalul de comenzi';
+$lng['ro']['w']['placeholder_order_telegram_chat'] = 'Chat ID canalul de comenzi';
+$lng['ro']['w']['placeholder_order_facebook_page'] = 'ID pagină Facebook pentru comenzi';
+$lng['ro']['w']['placeholder_order_facebook_token'] = 'Token acces pentru pagina de comenzi';
+
 // Russian
 $lng['ru']['w']['publication_settings_title'] = 'Настройки публикации автомобилей';
 $lng['ru']['w']['publication_settings_saved'] = 'Настройки публикации сохранены!';
 $lng['ru']['w']['cars_in_stock'] = 'Автомобили в наличии';
 $lng['ru']['w']['cars_on_order'] = 'Автомобили под заказ';
-$lng['ru']['w']['api_999md_main'] = 'API "Три Девятки МД" - Основной аккаунт';
-$lng['ru']['w']['api_999md_separate'] = 'API "Три Девятки МД" - Отдельный аккаунт';
+$lng['ru']['w']['api_999md_main'] = 'API "999.md" - Основной аккаунт';
+$lng['ru']['w']['api_999md_separate'] = 'API "999.md" - Отдельный аккаунт';
 $lng['ru']['w']['account_login'] = 'Аккаунт/Login:';
 $lng['ru']['w']['api_token'] = 'API Token:';
-$lng['ru']['w']['telegram_channel_1'] = 'Telegram - Канал №1';
-$lng['ru']['w']['telegram_separate'] = 'Telegram - Отдельный канал';
+$lng['ru']['w']['telegram_channel_1'] = 'Telegram';
+$lng['ru']['w']['telegram_separate'] = 'Telegram';
 $lng['ru']['w']['bot_token'] = 'Bot Token:';
 $lng['ru']['w']['chat_id'] = 'Chat ID:';
-$lng['ru']['w']['facebook_main'] = 'Facebook - Основная страница';
-$lng['ru']['w']['facebook_same'] = 'Facebook - Та же страница с меткой "Под заказ"';
+$lng['ru']['w']['facebook_main'] = 'Facebook';
+$lng['ru']['w']['facebook_same'] = 'Facebook';
 $lng['ru']['w']['page_id'] = 'Page ID:';
 $lng['ru']['w']['page_token'] = 'Page Token:';
 $lng['ru']['w']['auto_publish_stock'] = 'Автоматическая публикация при добавлении автомобиля в наличии';
@@ -66,21 +80,35 @@ $lng['ru']['w']['save_settings'] = 'Сохранить настройки';
 $lng['ru']['w']['separate_account'] = 'Отдельный аккаунт для заказов';
 $lng['ru']['w']['same_page_id'] = 'ID Facebook страницы для заказов';
 
+// Placeholder translations - Russian
+$lng['ru']['w']['placeholder_main_999md_account'] = 'Основной аккаунт 999.md';
+$lng['ru']['w']['placeholder_main_999md_token'] = 'API токен для основного аккаунта';
+$lng['ru']['w']['placeholder_main_telegram_token'] = 'Bot token для основного канала';
+$lng['ru']['w']['placeholder_main_telegram_chat'] = 'Chat ID основного канала';
+$lng['ru']['w']['placeholder_main_facebook_page'] = 'ID основной Facebook страницы';
+$lng['ru']['w']['placeholder_main_facebook_token'] = 'Access token для основной страницы';
+$lng['ru']['w']['placeholder_order_999md_account'] = 'Отдельный аккаунт для заказов';
+$lng['ru']['w']['placeholder_order_999md_token'] = 'API токен для аккаунта заказов';
+$lng['ru']['w']['placeholder_order_telegram_token'] = 'Bot token для канала заказов';
+$lng['ru']['w']['placeholder_order_telegram_chat'] = 'Chat ID канала заказов';
+$lng['ru']['w']['placeholder_order_facebook_page'] = 'ID Facebook страницы для заказов';
+$lng['ru']['w']['placeholder_order_facebook_token'] = 'Access token для страницы заказов';
+
 // English
 $lng['en']['w']['publication_settings_title'] = 'Car Publication Settings';
 $lng['en']['w']['publication_settings_saved'] = 'Publication settings saved!';
 $lng['en']['w']['cars_in_stock'] = 'Cars in Stock';
 $lng['en']['w']['cars_on_order'] = 'Cars on Order';
-$lng['en']['w']['api_999md_main'] = 'API "Три Девятки МД" - Main Account';
-$lng['en']['w']['api_999md_separate'] = 'API "Три Девятки МД" - Separate Account';
+$lng['en']['w']['api_999md_main'] = 'API "999.md" - Main Account';
+$lng['en']['w']['api_999md_separate'] = 'API "999.md" - Separate Account';
 $lng['en']['w']['account_login'] = 'Account/Login:';
 $lng['en']['w']['api_token'] = 'API Token:';
-$lng['en']['w']['telegram_channel_1'] = 'Telegram - Channel №1';
-$lng['en']['w']['telegram_separate'] = 'Telegram - Separate Channel';
+$lng['en']['w']['telegram_channel_1'] = 'Telegram';
+$lng['en']['w']['telegram_separate'] = 'Telegram';
 $lng['en']['w']['bot_token'] = 'Bot Token:';
 $lng['en']['w']['chat_id'] = 'Chat ID:';
-$lng['en']['w']['facebook_main'] = 'Facebook - Main Page';
-$lng['en']['w']['facebook_same'] = 'Facebook - Same page with "On Order" label';
+$lng['en']['w']['facebook_main'] = 'Facebook';
+$lng['en']['w']['facebook_same'] = 'Facebook';
 $lng['en']['w']['page_id'] = 'Page ID:';
 $lng['en']['w']['page_token'] = 'Page Token:';
 $lng['en']['w']['auto_publish_stock'] = 'Auto-publish when adding car in stock';
@@ -88,6 +116,20 @@ $lng['en']['w']['auto_publish_order'] = 'Auto-publish when adding car on order';
 $lng['en']['w']['save_settings'] = 'Save Settings';
 $lng['en']['w']['separate_account'] = 'Separate account for orders';
 $lng['en']['w']['same_page_id'] = 'Facebook page ID for orders';
+
+// Placeholder translations - English
+$lng['en']['w']['placeholder_main_999md_account'] = 'Main 999.md account';
+$lng['en']['w']['placeholder_main_999md_token'] = 'API token for main account';
+$lng['en']['w']['placeholder_main_telegram_token'] = 'Bot token for main channel';
+$lng['en']['w']['placeholder_main_telegram_chat'] = 'Chat ID for main channel';
+$lng['en']['w']['placeholder_main_facebook_page'] = 'Main Facebook page ID';
+$lng['en']['w']['placeholder_main_facebook_token'] = 'Access token for main page';
+$lng['en']['w']['placeholder_order_999md_account'] = 'Separate account for orders';
+$lng['en']['w']['placeholder_order_999md_token'] = 'API token for orders account';
+$lng['en']['w']['placeholder_order_telegram_token'] = 'Bot token for orders channel';
+$lng['en']['w']['placeholder_order_telegram_chat'] = 'Chat ID for orders channel';
+$lng['en']['w']['placeholder_order_facebook_page'] = 'Facebook page ID for orders';
+$lng['en']['w']['placeholder_order_facebook_token'] = 'Access token for orders page';
 
 
 // Publication Settings for Order Cars vs Regular Cars
@@ -141,33 +183,33 @@ $rtrn .= '
     
     <form method="POST">
         <div class="settings-section">
-            <h3>🚗 ' . ($lng[$_COOKIE['lang']]['w']['cars_in_stock'] ?? 'Автомобили в наличии') . ' (catalog_type = "in_stock")</h3>
+            <h3>🚗 ' . ($lng[$_COOKIE['lang']]['w']['cars_in_stock'] ?? 'Автомобили в наличии') . '</h3>
             
             <div class="setting-group">
                 <h4>' . ($lng[$_COOKIE['lang']]['w']['api_999md_main'] ?? 'API "Три Девятки МД" - Основной аккаунт') . '</h4>
                 <label>' . ($lng[$_COOKIE['lang']]['w']['account_login'] ?? 'Аккаунт/Login:') . '</label>
-                <input type="text" name="regular_999md_account" value="' . htmlspecialchars($current_settings['regular_999md_account'] ?? '') . '" placeholder="Основной аккаунт 999.md">
+                <input type="text" name="regular_999md_account" value="' . htmlspecialchars($current_settings['regular_999md_account'] ?? '') . '" placeholder="' . ($lng[$_COOKIE['lang']]['w']['placeholder_main_999md_account'] ?? 'Основной аккаунт 999.md') . '">
                 
                 <label>' . ($lng[$_COOKIE['lang']]['w']['api_token'] ?? 'API Token:') . '</label>
-                <input type="password" name="regular_999md_token" value="' . htmlspecialchars($current_settings['regular_999md_token'] ?? '') . '" placeholder="API токен для основного аккаунта">
+                <input type="password" name="regular_999md_token" value="' . htmlspecialchars($current_settings['regular_999md_token'] ?? '') . '" placeholder="' . ($lng[$_COOKIE['lang']]['w']['placeholder_main_999md_token'] ?? 'API токен для основного аккаунта') . '">
             </div>
             
             <div class="setting-group">
                 <h4>' . ($lng[$_COOKIE['lang']]['w']['telegram_channel_1'] ?? 'Telegram - Канал №1') . '</h4>
                 <label>' . ($lng[$_COOKIE['lang']]['w']['bot_token'] ?? 'Bot Token:') . '</label>
-                <input type="password" name="regular_telegram_bot_token" value="' . htmlspecialchars($current_settings['regular_telegram_bot_token'] ?? '7459955785:AAGTMPvUkh2Fktar7ZpNlHBFsq43FH_DPsY') . '" placeholder="Bot token для основного канала">
+                <input type="password" name="regular_telegram_bot_token" value="' . htmlspecialchars($current_settings['regular_telegram_bot_token'] ?? '7459955785:AAGTMPvUkh2Fktar7ZpNlHBFsq43FH_DPsY') . '" placeholder="' . ($lng[$_COOKIE['lang']]['w']['placeholder_main_telegram_token'] ?? 'Bot token для основного канала') . '">
                 
                 <label>' . ($lng[$_COOKIE['lang']]['w']['chat_id'] ?? 'Chat ID:') . '</label>
-                <input type="text" name="regular_telegram_chat_id" value="' . htmlspecialchars($current_settings['regular_telegram_chat_id'] ?? '-1002605369940') . '" placeholder="Chat ID основного канала">
+                <input type="text" name="regular_telegram_chat_id" value="' . htmlspecialchars($current_settings['regular_telegram_chat_id'] ?? '-1002605369940') . '" placeholder="' . ($lng[$_COOKIE['lang']]['w']['placeholder_main_telegram_chat'] ?? 'Chat ID основного канала') . '">
             </div>
             
             <div class="setting-group">
                 <h4>' . ($lng[$_COOKIE['lang']]['w']['facebook_main'] ?? 'Facebook - Основная страница') . '</h4>
                 <label>' . ($lng[$_COOKIE['lang']]['w']['page_id'] ?? 'Page ID:') . '</label>
-                <input type="text" name="regular_facebook_page_id" value="' . htmlspecialchars($current_settings['regular_facebook_page_id'] ?? '482777831588669') . '" placeholder="ID основной Facebook страницы">
+                <input type="text" name="regular_facebook_page_id" value="' . htmlspecialchars($current_settings['regular_facebook_page_id'] ?? '482777831588669') . '" placeholder="' . ($lng[$_COOKIE['lang']]['w']['placeholder_main_facebook_page'] ?? 'ID основной Facebook страницы') . '">
                 
                 <label>' . ($lng[$_COOKIE['lang']]['w']['page_token'] ?? 'Page Token:') . '</label>
-                <input type="password" name="regular_facebook_token" value="' . htmlspecialchars($current_settings['regular_facebook_token'] ?? '') . '" placeholder="Access token для основной страницы">
+                <input type="password" name="regular_facebook_token" value="' . htmlspecialchars($current_settings['regular_facebook_token'] ?? '') . '" placeholder="' . ($lng[$_COOKIE['lang']]['w']['placeholder_main_facebook_token'] ?? 'Access token для основной страницы') . '">
             </div>
             
             <div class="setting-group">
@@ -179,33 +221,33 @@ $rtrn .= '
         </div>
         
         <div class="settings-section">
-            <h3>📋 ' . ($lng[$_COOKIE['lang']]['w']['cars_on_order'] ?? 'Автомобили под заказ') . ' (catalog_type = "on_order")</h3>
+            <h3>📋 ' . ($lng[$_COOKIE['lang']]['w']['cars_on_order'] ?? 'Автомобили под заказ') . '</h3>
             
             <div class="setting-group">
                 <h4>' . ($lng[$_COOKIE['lang']]['w']['api_999md_separate'] ?? 'API "Три Девятки МД" - Отдельный аккаунт') . '</h4>
                 <label>' . ($lng[$_COOKIE['lang']]['w']['account_login'] ?? 'Аккаунт/Login:') . '</label>
-                <input type="text" name="order_999md_account" value="' . htmlspecialchars($current_settings['order_999md_account'] ?? '') . '" placeholder="' . ($lng[$_COOKIE['lang']]['w']['separate_account'] ?? 'Отдельный аккаунт для заказов') . '">
+                <input type="text" name="order_999md_account" value="' . htmlspecialchars($current_settings['order_999md_account'] ?? '') . '" placeholder="' . ($lng[$_COOKIE['lang']]['w']['placeholder_order_999md_account'] ?? 'Отдельный аккаунт для заказов') . '">
                 
                 <label>' . ($lng[$_COOKIE['lang']]['w']['api_token'] ?? 'API Token:') . '</label>
-                <input type="password" name="order_999md_token" value="' . htmlspecialchars($current_settings['order_999md_token'] ?? '') . '" placeholder="' . ($lng[$_COOKIE['lang']]['w']['api_token'] ?? 'API токен для аккаунта заказов') . '">
+                <input type="password" name="order_999md_token" value="' . htmlspecialchars($current_settings['order_999md_token'] ?? '') . '" placeholder="' . ($lng[$_COOKIE['lang']]['w']['placeholder_order_999md_token'] ?? 'API токен для аккаунта заказов') . '">
             </div>
             
             <div class="setting-group">
                 <h4>' . ($lng[$_COOKIE['lang']]['w']['telegram_separate'] ?? 'Telegram - Отдельный канал') . '</h4>
                 <label>' . ($lng[$_COOKIE['lang']]['w']['bot_token'] ?? 'Bot Token:') . '</label>
-                <input type="password" name="order_telegram_bot_token" value="' . htmlspecialchars($current_settings['order_telegram_bot_token'] ?? '') . '" placeholder="' . ($lng[$_COOKIE['lang']]['w']['bot_token'] ?? 'Bot token для канала заказов') . '">
+                <input type="password" name="order_telegram_bot_token" value="' . htmlspecialchars($current_settings['order_telegram_bot_token'] ?? '') . '" placeholder="' . ($lng[$_COOKIE['lang']]['w']['placeholder_order_telegram_token'] ?? 'Bot token для канала заказов') . '">
                 
                 <label>' . ($lng[$_COOKIE['lang']]['w']['chat_id'] ?? 'Chat ID:') . '</label>
-                <input type="text" name="order_telegram_chat_id" value="' . htmlspecialchars($current_settings['order_telegram_chat_id'] ?? '') . '" placeholder="' . ($lng[$_COOKIE['lang']]['w']['chat_id'] ?? 'Chat ID канала заказов') . '">
+                <input type="text" name="order_telegram_chat_id" value="' . htmlspecialchars($current_settings['order_telegram_chat_id'] ?? '') . '" placeholder="' . ($lng[$_COOKIE['lang']]['w']['placeholder_order_telegram_chat'] ?? 'Chat ID канала заказов') . '">
             </div>
             
             <div class="setting-group">
                 <h4>' . ($lng[$_COOKIE['lang']]['w']['facebook_same'] ?? 'Facebook - Та же страница с меткой "Под заказ"') . '</h4>
                 <label>' . ($lng[$_COOKIE['lang']]['w']['page_id'] ?? 'Page ID:') . '</label>
-                <input type="text" name="order_facebook_page_id" value="' . htmlspecialchars($current_settings['order_facebook_page_id'] ?? '482777831588669') . '" placeholder="' . ($lng[$_COOKIE['lang']]['w']['same_page_id'] ?? 'ID Facebook страницы для заказов') . '">
+                <input type="text" name="order_facebook_page_id" value="' . htmlspecialchars($current_settings['order_facebook_page_id'] ?? '482777831588669') . '" placeholder="' . ($lng[$_COOKIE['lang']]['w']['placeholder_order_facebook_page'] ?? 'ID Facebook страницы для заказов') . '">
                 
                 <label>' . ($lng[$_COOKIE['lang']]['w']['page_token'] ?? 'Page Token:') . '</label>
-                <input type="password" name="order_facebook_token" value="' . htmlspecialchars($current_settings['order_facebook_token'] ?? '') . '" placeholder="' . ($lng[$_COOKIE['lang']]['w']['page_token'] ?? 'Access token для страницы заказов') . '">
+                <input type="password" name="order_facebook_token" value="' . htmlspecialchars($current_settings['order_facebook_token'] ?? '') . '" placeholder="' . ($lng[$_COOKIE['lang']]['w']['placeholder_order_facebook_token'] ?? 'Access token для страницы заказов') . '">
             </div>
             
             <div class="setting-group">
