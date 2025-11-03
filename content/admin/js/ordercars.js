c2b5b5b17821bd031a90d77cf312825209ee71bf
GrigoreBotnarenco <botnarenco1996@mail.com>
2025-11-04 00:13:07 +0200

    update model

--- content/admin/js/ordercars.js
+++ content/admin/js/ordercars.js
@@ -252,7 +252,7 @@ $(document).ready(function(){
 			console.log('Syncing brand from SAUTO to 999:', brandValue);
 			$('#main_form_999').find('select[name="feature[20]"]').val(brandValue);
 		}
-	}).on('change', '#content_box .model', function(){
+	}).on('change', 'select[name="mo"]', function(){
 		// Sync model from SAUTO to 999 form
 		const modelValue = $(this).val();
 		if (modelValue) {
