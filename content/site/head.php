56ca8aeb53b35b82e5fed892c5d2da00e1fd4a7f
GrigoreBotnarenco <botnarenco1996@mail.com>
2025-11-08 19:27:50 +0200

    delete jivo

--- content/site/head.php
+++ content/site/head.php
@@ -290,7 +290,6 @@ $(document).ready(function(){
 
 <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
 <script async src="https://cdn.ampproject.org/v0.js"></script>
-<script src="//code.jivosite.com/widget/TZSdzj6D1H" async></script>
 
 <!-- Facebook Pixel Code -->
 <script>
