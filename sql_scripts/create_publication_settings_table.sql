23993f277383b7ee28c709310e2e86e62489bf2a
GrigoreBotnarenco <botnarenco1996@mail.com>
2025-11-11 23:17:24 +0200

    update

--- sql_scripts/create_publication_settings_table.sql
+++ sql_scripts/create_publication_settings_table.sql
@@ -14,12 +14,12 @@ INSERT INTO `gh3sp_settings` (`name`, `value`) VALUES
 ('location_2_facebook_token', 'EAAPYJ3JWk0UBPhHoFrglY8vNF9Jrm40RdjCvuPkYB0mO226K3yqF5qQrZAUasvkmAidLqK87dTZCCRyVwpMReuR5EKscMKwJjoAFZAiTUwjMSjLdstz15BmWr6QQfJR8YBKZAkBy0ksMHXwdvL8vzZAUpoF5K7osglWrLVQZB91xbtFJnUkw2MZCMhZAB6YRMATSJc46'),
 
 -- 999.md API settings
--- Account 1: Sauto-auto-comerciale (pentru mașini in_stock - promovare personalizată)
+-- Account 1: Sauto-auto-comerciale (pentru mașini in_stock - promovare personalizată cu numărul 37379600616)
 ('regular_999md_account', 'Sauto-auto-comerciale'),
 ('regular_999md_token', 'EeKkPqGFjEhJZIK3S5KWh59w8jNG'),
--- Account 2: SAUTO-HAUS (backup, dacă este necesar)
-('comerciale_999md_account', 'SAUTO-HAUS'),
-('comerciale_999md_token', 'I_SKyGEvvG5Rfm7lRZeiOTwk7r_F'),
+-- Account 2: SAUTO-HAUS (păstrat pentru alte mașini dacă este necesar)
+('sautohaus_999md_account', 'SAUTO-HAUS'),
+('sautohaus_999md_token', 'I_SKyGEvvG5Rfm7lRZeiOTwk7r_F'),
 -- Account 3: Sauto-stock-extern (pentru mașini on_order)
 ('order_999md_account', 'Sauto-stock-extern'),
 ('order_999md_token', 'jMEsHjO0FhoRZm0KSsONLpkGLMIK'),
