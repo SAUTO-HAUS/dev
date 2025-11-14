2c4ef44c0fa0b4839dd2dda6c0fd42569cbd2a23
GrigoreBotnarenco <botnarenco1996@mail.com>
2025-11-14 21:18:53 +0200

    update telegram

--- sql_scripts/create_publication_settings_table.sql
+++ sql_scripts/create_publication_settings_table.sql
@@ -2,8 +2,8 @@ INSERT INTO `gh3sp_settings` (`name`, `value`) VALUES
 -- Telegram settings (using working values from old code)
 ('regular_telegram_bot_token', '7459955785:AAGTMPvUkh2Fktar7ZpNlHBFsq43FH_DPsY'),
 ('regular_telegram_chat_id', '-1002605369940'),
-('order_telegram_bot_token', '8169302156:AAEe1j7AASXegfKRdWB-rSiaKY-PSgqkGgo'),
-('order_telegram_chat_id', '-1002605369940'),
+('order_telegram_bot_token', '8076798469:AAELFExQ8nu5F5Rm6C3OJvIoLyRYYTPQmTI'),
+('order_telegram_chat_id', '-1002398874348'),
 
 -- Facebook settings (location-based - ONLY by address, not catalog type)
 -- str. Calea Moşilor 11 (location ID: 1) - Subdomain (corrected mapping)
