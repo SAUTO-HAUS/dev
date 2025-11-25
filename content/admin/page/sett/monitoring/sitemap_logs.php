ec8b0d1f64c370460879506d7faa3079004097b2
GrigoreBotnarenco <botnarenco1996@mail.com>
2025-11-25 18:56:10 +0200

    update

--- content/admin/page/sett/monitoring/sitemap_logs.php
+++ content/admin/page/sett/monitoring/sitemap_logs.php
@@ -1,6 +1,5 @@
 <?php
 echo '
-<h2>Sitemap Logs</h2>
 <iframe src="/new_sitemap/view_logs.php?pass=sauto2025&refresh=30" style="width:100%; height:800px; border:1px solid #ccc; border-radius:8px;"></iframe>
 ';
 ?>
