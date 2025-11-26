c5b1fa948fb31880f8fcd959624700581536a908
GrigoreBotnarenco <botnarenco1996@mail.com>
2025-11-26 21:54:10 +0200

    update

--- content/admin/page/docs.php
+++ content/admin/page/docs.php
@@ -706,6 +706,7 @@ c/f 1017600006845, c/TVA 0609417</pre>
 				$rtrn .= '<script>window.adm_ar = '.json_encode($adm_ar).';</script>';
 				
 				$i=1; $date = '';
+				$currentYear = date('Y');
 				$pdo = $db->prepare('SELECT 
 					u.id u_id, u.nm u_nm, u.tp u_tp, u.cf_idno u_cf_idno, u.tva_dt u_tva_dt, u.iban_dt_tk u_iban_dt_tk, u.adr u_adr, u.phn u_phn, u.eml u_eml, 
 					c.*, 
@@ -714,7 +715,10 @@ c/f 1017600006845, c/TVA 0609417</pre>
 						'.$prefx.'_docs_u AS u 
 						INNER JOIN 
 						'.$prefx.'_docs_ctlg AS c 
-					ON u.id=c.u ORDER BY c.date DESC, c.id DESC'); $pdo->execute();
+					ON u.id=c.u 
+					WHERE YEAR(c.date) = :currentYear
+					ORDER BY c.date DESC, c.id DESC'); 
+				$pdo->execute(['currentYear' => $currentYear]);
 					
 				$rtrn .= '
 				<div class="rowz hdr">
