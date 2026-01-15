40cfe2e2e00ed0b9b4e3cada821e0732bfb3372a
GrigoreBotnarenco <botnarenco1996@mail.com>
2026-01-16 01:02:35 +0200

    update

--- content/admin/include/docs/cars/foaie_parcurs.php
+++ content/admin/include/docs/cars/foaie_parcurs.php
@@ -10,6 +10,36 @@ if (isset($cont_n)) {
 	$fp_nr = $fp_start_nr;
 }
 
+// Parse autovehicul to extract marca and nr inmatriculare
+$autovehicul_marca = '';
+$autovehicul_nr = '';
+$remorca_marca = '';
+$remorca_nr = '';
+if (isset($_POST['autovehicul']) && !empty($_POST['autovehicul'])) {
+	// Format: "MERCEDES ACTROS - SMM149 | KASSBORHER - X239XC"
+	$parts = explode(' | ', $_POST['autovehicul']);
+	if (isset($parts[0])) {
+		$auto_parts = explode(' - ', $parts[0]);
+		if (isset($auto_parts[0])) {
+			$autovehicul_marca = trim($auto_parts[0]); // MERCEDES ACTROS
+		}
+		if (isset($auto_parts[1])) {
+			$autovehicul_nr = trim($auto_parts[1]); // SMM149
+		}
+	}
+	if (isset($parts[1])) {
+		$remorca_parts = explode(' - ', $parts[1]);
+		if (isset($remorca_parts[0])) {
+			$remorca_marca = trim($remorca_parts[0]); // KASSBORHER
+		}
+		if (isset($remorca_parts[1])) {
+			$remorca_nr = trim($remorca_parts[1]); // X239XC
+		}
+	}
+}
+
+$sofer_nume = isset($_POST['sofer']) ? $_POST['sofer'] : '';
+
 $rtrn = '
 <style>
 	.fp_page {font-family:"Times New Roman", Times, serif !important; background-color:#f0f0f0 !important;}
@@ -51,10 +81,55 @@ $rtrn = '
 				<tr>
 					<td style="width:20%; border:1px solid #000; padding:1mm; font-size:0.65rem;"><div>Marca autovehiculului, remorcii (semiremorcii)</div><div>Марка автомобиля, прицепа (полуприцепа)</div></td>
 					<td style="width:10%; border:1px solid #000; padding:1mm; font-size:0.65rem;"><div>Nr. de înmatriculare</div><div>Регистрационный номер</div></td>
-					<td style="width:10%; border:1px solid #000; border-right:1px solid #fff; padding:1mm; font-size:0.65rem;"><div>Nr. inventar</div><div>Гаражный №</div></td>
-					<td style="width:25%; border:1px solid #000; border-left:1px solid #fff; padding:1mm; font-size:0.65rem;"><div>Numele şi prenumele şoferilor</div><div>Фамилия и имя водителей</div></td>
+					<td style="width:10%; border:1px solid #000; border-right:0px solid; padding:1mm; font-size:0.65rem;"><div>Nr. inventar</div><div>Гаражный №</div></td>
+					<td style="width:25%; border:1px solid #000; border-left:0px solid; padding:1mm; font-size:0.65rem;"><div>Numele şi prenumele şoferilor</div><div>Фамилия и имя водителей</div></td>
 					<td style="width:10%; border:1px solid #000; padding:1mm; font-size:0.65rem;"><div>Nr. matricol</div><div>Табельный №</div></td>
 					<td style="width:25%; border:1px solid #000; padding:1mm; font-size:0.65rem;"><div>Starea sănătăţii şoferului</div><div>Состояние здоровья водителя</div></td>
+				</tr>
+			</table>
+			
+			<!-- Rând 3: 6 coloane cu cifre 3-8 -->
+			<table style="width:100%; border-collapse:collapse; margin-top:0;">
+				<tr>
+					<td style="width:20%; border:1px solid #000; border-top:1px solid #fff; padding:1mm; text-align:center; font-size:0.7rem; font-weight:bold;">3</td>
+					<td style="width:10%; border:1px solid #000; border-top:1px solid #fff; padding:1mm; text-align:center; font-size:0.7rem; font-weight:bold;">4</td>
+					<td style="width:10%; border:1px solid #000; border-top:1px solid #fff; border-right:0px solid; padding:1mm; text-align:center; font-size:0.7rem; font-weight:bold;">5</td>
+					<td style="width:25%; border:1px solid #000; border-top:1px solid #fff; border-left:0px solid; padding:1mm; text-align:center; font-size:0.7rem; font-weight:bold;">6</td>
+					<td style="width:10%; border:1px solid #000; border-top:1px solid #fff; padding:1mm; text-align:center; font-size:0.7rem; font-weight:bold;">7</td>
+					<td style="width:25%; border:1px solid #000; border-top:1px solid #fff; padding:1mm; text-align:center; font-size:0.7rem; font-weight:bold;">8</td>
+				</tr>
+			</table>
+			
+			<!-- Rând 4: 6 coloane cu date din formular -->
+			<table style="width:100%; border-collapse:collapse; margin-top:0;">
+				<tr>
+					<td style="width:20%; border:1px solid #000; border-top:1px solid #fff; padding:1mm; font-size:0.7rem;">'.$autovehicul_marca.'</td>
+					<td style="width:10%; border:1px solid #000; border-top:1px solid #fff; padding:1mm; font-size:0.7rem;">'.$autovehicul_nr.'</td>
+					<td style="width:10%; border:1px solid #000; border-top:1px solid #fff; border-right:0px solid; padding:1mm; font-size:0.7rem;"></td>
+					<td style="width:25%; border:1px solid #000; border-top:1px solid #fff; border-left:0px solid; padding:1mm; font-size:0.7rem;">'.$sofer_nume.'</td>
+					<td style="width:10%; border:1px solid #000; border-top:1px solid #fff; padding:1mm; font-size:0.7rem;"></td>
+					<td style="width:25%; border:1px solid #000; border-top:1px solid #fff; border-bottom:1px solid #fff; padding:1mm; font-size:0.7rem;"></td>
+				</tr>
+			</table>
+			
+			<!-- Rând 5: 6 coloane cu date remorca -->
+			<table style="width:100%; border-collapse:collapse; margin-top:0;">
+				<tr>
+					<td style="width:20%; border:1px solid #000; border-top:1px solid #fff; padding:1mm; font-size:0.7rem;">'.$remorca_marca.'</td>
+					<td style="width:10%; border:1px solid #000; border-top:1px solid #fff; padding:1mm; font-size:0.7rem;">'.$remorca_nr.'</td>
+					<td style="width:10%; border:1px solid #000; border-top:1px solid #fff; border-right:0px solid; padding:1mm; font-size:0.7rem;"></td>
+					<td style="width:25%; border:1px solid #000; border-top:1px solid #fff; border-left:0px solid; padding:1mm; font-size:0.7rem;"></td>
+					<td style="width:10%; border:1px solid #000; border-top:1px solid #fff; padding:1mm; font-size:0.7rem;"></td>
+					<td style="width:25%; border:1px solid #000; border-top:1px solid #fff; border-bottom:1px solid #fff; padding:1mm; font-size:0.7rem;"></td>
+				</tr>
+			</table>
+			
+			<!-- Rând 6: 3 coloane - cifra 9, sarcina soferului, gol -->
+			<table style="width:100%; border-collapse:collapse; margin-top:0;">
+				<tr>
+					<td style="width:3.5%; border:1px solid #000; border-top:1px solid #fff; padding:1mm; text-align:center; font-size:0.7rem; font-weight:bold;">9</td>
+					<td style="width:71.5%; border:1px solid #000; border-top:1px solid #fff; padding:1mm; font-size:0.7rem;"><div>Sarcina şoferului:</div><div>Задание водителю:</div></td>
+					<td style="width:25%; border:1px solid #000; border-top:1px solid #fff; padding:1mm; font-size:0.7rem;"></td>
 				</tr>
 			</table>
 		</div>
