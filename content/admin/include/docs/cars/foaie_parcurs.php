c0659d63e0a9c568bf5803554a5d5ada0ce59213
GrigoreBotnarenco <botnarenco1996@mail.com>
2026-01-16 01:49:19 +0200

    update

--- content/admin/include/docs/cars/foaie_parcurs.php
+++ content/admin/include/docs/cars/foaie_parcurs.php
@@ -196,6 +196,24 @@ $rtrn = '
 					<td style="width:25%; border:1px solid #000; border-top:1px solid #fff; padding:1mm; text-align:center; font-size:0.6rem; font-weight:bold;"></td>
 				</tr>
 			</table>
+			
+			<!-- Rând 12: 7 coloane - col 1 "La plecare / При выезде", restul goale -->
+			<table style="width:100%; border-collapse:collapse; margin-top:0;">
+				<tr>
+					<td style="width:9%; border:1px solid #000; border-top:1px solid #fff; padding:1mm; font-size:0.6rem;"><div>La plecare</div><div>При выезде</div></td>
+					<td style="width:9%; border:1px solid #000; border-top:1px solid #fff; padding:1mm; text-align:center; font-size:0.6rem; font-weight:bold;"></td>
+					<td style="width:9%; border:1px solid #000; border-top:1px solid #fff; padding:1mm; text-align:center; font-size:0.6rem; font-weight:bold;"></td>
+					<td style="width:11.4%; border:1px solid #000; border-top:1px solid #fff; padding:1mm; text-align:center; font-size:0.6rem; font-weight:bold;"></td>
+					<td style="width:11.4%; border:1px solid #000; border-top:1px solid #fff; padding:1mm; text-align:center; font-size:0.6rem; font-weight:bold;"></td>
+					<td style="width:25.2%; border:1px solid #000; border-top:1px solid #fff; padding:0; font-size:0.6rem;">
+						<div style="display:flex; height:8mm">
+							<div style="width:50%; border-right:1px solid #000; padding:1mm; text-align:center; font-weight:bold; display:flex; align-items:center; justify-content:center;"></div>
+							<div style="width:50%; padding:1mm; text-align:center; font-weight:bold; display:flex; align-items:center; justify-content:center;"></div>
+						</div>
+					</td>
+					<td style="width:25%; border:1px solid #000; border-top:1px solid #fff; padding:1mm; text-align:center; font-size:0.6rem; font-weight:bold;"></td>
+				</tr>
+			</table>
 		</div>
 	</div>
 </div>';
