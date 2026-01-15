4edb610bdf966fd6a8914e98eafe30b6e5e5263f
GrigoreBotnarenco <botnarenco1996@mail.com>
2026-01-15 23:00:11 +0200

    update

--- content/admin/include/docs/cars/foaie_parcurs.php
+++ content/admin/include/docs/cars/foaie_parcurs.php
@@ -4,10 +4,14 @@ $abr = 'FP';
 
 $rtrn = '
 <style>
-
-</style>
+	.fp_page {font-family:"Times New Roman", Times, serif !important; background-color:#f0f0f0 !important;}
+	.fp_page .top-right {position:absolute; top:5mm; right:5mm; text-align:right; font-size:0.7rem; line-height:1.4;}
+	</style>
 <div id="p_cont" class="base">
-	<div class="pg">
+	<div class="pg fp_page">
+		<div class="top-right">Formular tipizat<br>Типовая форма</div>
+		
+		<div style="text-align:center; font-size:0.65rem;">Aprobat prin Ordinul comun al Departamentului Statisticii al Republicii Moldova şi Ministerului Finantelor al Republicii Moldova nr.24/36 din 25 martie 1998<br>Утверждена совместным приказом Департамента статистики и Министерства финансов Республики Молдова № 24/36 от 25 марта 1998 г.</div>
 	</div>
 </div>';
 
