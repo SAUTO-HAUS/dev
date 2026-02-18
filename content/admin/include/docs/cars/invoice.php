3dde685918b3628ea0090edd092fde5f5b04cbac
GrigoreBotnarenco <botnarenco1996@mail.com>
2026-02-18 23:39:06 +0200

    update

--- content/admin/include/docs/cars/invoice.php
+++ content/admin/include/docs/cars/invoice.php
@@ -60,11 +60,11 @@ if ($custom_description && $brand_model) {
 }
 
 $rtrn .= '
-<div class="pg bg" style="font-size: 16px; line-height: 1.3; padding: 10px 30px;">
-	<div style="text-align: left; font-size: 36px; font-weight: bold; margin-bottom: 15px;">INVOICE</div>
+<div class="pg bg" style="font-size: 15px; line-height: 1.3; padding: 9px 29px;">
+	<div style="text-align: left; font-size: 35px; font-weight: bold; margin-bottom: 14px;">INVOICE</div>
 	
-	<div style="display: flex; margin-bottom: 15px;">
-		<div style="width: 50%; padding-right: 15px;">
+	<div style="display: flex; margin-bottom: 14px;">
+		<div style="width: 50%; padding-right: 14px;">
 		<div style="font-size: 14px; color: #666; margin-bottom: 8px;">Vinzator:</div>
 			<div style="font-weight: bold; font-size: 22px; margin-bottom: 3px;">Verkäufer:</div>
 			
@@ -76,7 +76,7 @@ $rtrn .= '
 				'.($seller_swift ? '<div>'.$seller_swift.'</div>' : '').'
 			</div>
 		</div>
-		<div style="width: 50%; padding-left: 15px;">
+		<div style="width: 50%; padding-left: 14px;">
 		<div style="font-size: 14px; color: #666; margin-bottom: 8px;">Cumparator:</div>
 			<div style="font-weight: bold; font-size: 22px; margin-bottom: 3px;">Käufer:</div>
 			
@@ -90,78 +90,78 @@ $rtrn .= '
 		</div>
 	</div>
 	
-	<div style="text-align: center; margin: 20px 0;">
+	<div style="text-align: center; margin: 19px 0;">
 		<div style="font-weight: bold; font-size: 26px; margin: 5px 0;">Rechnung/Kaufvertrag</div>
 	</div>
 	
-	<table style="width: 100%; margin: 15px 0; border-collapse: collapse; border: 2px solid #000;">
+	<table style="width: 100%; margin: 14px 0; border-collapse: collapse; border: 2px solid #000;">
 		<tr>
-			<td style="width: 33%; padding: 12px; border: 1px solid #000; font-size: 17px; font-weight: bold;">
+			<td style="width: 33%; padding: 11px; border: 1px solid #000; font-size: 16px; font-weight: bold;">
 				Rechnungsnummer<br>
 				<span style="font-size: 12px; font-weight: normal;">Număr de factură</span>
 			</td>
-			<td style="width: 33%; padding: 12px; border: 1px solid #000; font-size: 17px; font-weight: bold;">
+			<td style="width: 33%; padding: 11px; border: 1px solid #000; font-size: 16px; font-weight: bold;">
 				Interne Nummer<br>
 				<span style="font-size: 12px; font-weight: normal;">Număr intern</span>
 			</td>
-			<td style="width: 33%; padding: 12px; border: 1px solid #000; font-size: 17px; font-weight: bold;">
+			<td style="width: 33%; padding: 11px; border: 1px solid #000; font-size: 16px; font-weight: bold;">
 				Kundennummer<br>
 				<span style="font-size: 12px; font-weight: normal;">Numărul clientului</span>
 			</td>
 		</tr>
 		<tr>
-			<td style="padding: 12px; border: 1px solid #000; font-size: 15px;">
+			<td style="padding: 11px; border: 1px solid #000; font-size: 14px;">
 				<strong>Date:</strong> '.date('n/j/Y').'
 			</td>
-			<td style="padding: 12px; border: 1px solid #000; font-size: 15px;">
+			<td style="padding: 11px; border: 1px solid #000; font-size: 14px;">
 				<strong>'.$abr.$cont_y.$cont_q.'/'.$cont_n.'</strong>
 			</td>
-			<td style="padding: 12px; border: 1px solid #000; font-size: 15px;">
+			<td style="padding: 11px; border: 1px solid #000; font-size: 14px;">
 				
 			</td>
 		</tr>
 	</table>
 	
-	<table style="width: 100%; border-collapse: collapse; margin: 20px 0; border: 2px solid #000;">
+	<table style="width: 100%; border-collapse: collapse; margin: 19px 0; border: 2px solid #000;">
 		<thead>
 			<tr>
-				<th style="border: 1px solid #000; padding: 15px; text-align: left; font-size: 17px; font-weight: bold;">#</th>
-				<th style="border: 1px solid #000; padding: 15px; text-align: left; font-size: 17px; font-weight: bold;">Beschreibung<br><span style="font-size: 12px; font-weight: normal;">Descriere</span></th>
-				<th style="border: 1px solid #000; padding: 15px; text-align: left; font-size: 17px; font-weight: bold;">Anzahl<br><span style="font-size: 12px; font-weight: normal;">Cantitate</span></th>
-				<th style="border: 1px solid #000; padding: 15px; text-align: left; font-size: 17px; font-weight: bold;">Gesamtpreis<br><span style="font-size: 12px; font-weight: normal;">Preț total</span></th>
+				<th style="border: 1px solid #000; padding: 14px; text-align: left; font-size: 16px; font-weight: bold;">#</th>
+				<th style="border: 1px solid #000; padding: 14px; text-align: left; font-size: 16px; font-weight: bold;">Beschreibung<br><span style="font-size: 12px; font-weight: normal;">Descriere</span></th>
+				<th style="border: 1px solid #000; padding: 14px; text-align: left; font-size: 16px; font-weight: bold;">Anzahl<br><span style="font-size: 12px; font-weight: normal;">Cantitate</span></th>
+				<th style="border: 1px solid #000; padding: 14px; text-align: left; font-size: 16px; font-weight: bold;">Gesamtpreis<br><span style="font-size: 12px; font-weight: normal;">Preț total</span></th>
 			</tr>
 		</thead>
 		<tbody>
 			'.($description || $price > 0 ? '
 			<tr>
-				<td style="border: 1px solid #000; padding: 15px; font-size: 17px; font-weight: bold;">1</td>
-				<td style="border: 1px solid #000; padding: 15px; font-size: 17px;">'.$description.($vin ? '<br><em>VIN: '.$vin.'</em>' : '').'</td>
-				<td style="border: 1px solid #000; padding: 15px; font-size: 17px; text-align: center; font-weight: bold;">1</td>
-				<td style="border: 1px solid #000; padding: 15px; font-size: 20px; text-align: right; font-weight: bold;">'.number_format($price, 0).' '.$currency.'</td>
+				<td style="border: 1px solid #000; padding: 14px; font-size: 16px; font-weight: bold;">1</td>
+				<td style="border: 1px solid #000; padding: 14px; font-size: 16px;">'.$description.($vin ? '<br><em>VIN: '.$vin.'</em>' : '').'</td>
+				<td style="border: 1px solid #000; padding: 14px; font-size: 16px; text-align: center; font-weight: bold;">1</td>
+				<td style="border: 1px solid #000; padding: 14px; font-size: 19px; text-align: right; font-weight: bold;">'.number_format($price, 0).' '.$currency.'</td>
 			</tr>' : '').'
 		</tbody>
 	</table>
 	
-	<div style="margin: 10px 0; padding: 10px; border: 2px solid #000;">
-		<div style="display: flex; justify-content: space-between; margin: 8px 0; font-size: 17px;"><span><strong>Summe der Nettobetrage</strong></span><span></span></div>
-		<div style="display: flex; justify-content: space-between; margin: 8px 0; font-size: 11px;"><span><strong>Suma NETO</strong></span><span></span></div>
-		<div style="display: flex; justify-content: space-between; margin: 5px 0; font-size: 17px;"><span>*zzgl. 0% Umsatzsteuer</span><span></span></div>
-		<div style="display: flex; justify-content: space-between; margin: 5px 0; font-size: 11px;"><span>TVA</span><span></span></div>
-		<hr style="border: none; height: 2px; background: #000; margin: 15px 0;">
-		<div style="display: flex; justify-content: space-between; margin: 8px 0; font-size: 17px;"><span><strong>Gesamtbetrag</strong></span><span></span></div>
-		<div style="display: flex; justify-content: space-between; margin: 8px 0;"><span style="font-size: 17px;"><strong>Total</strong></span><span style="font-size: 20px;"><strong>'.number_format($price, 0).' '.$currency.'</strong></span></div>
+	<div style="margin: 9px 0; padding: 9px; border: 2px solid #000;">
+		<div style="display: flex; justify-content: space-between; margin: 7px 0; font-size: 16px;"><span><strong>Summe der Nettobetrage</strong></span><span></span></div>
+		<div style="display: flex; justify-content: space-between; margin: 7px 0; font-size: 10px;"><span><strong>Suma NETO</strong></span><span></span></div>
+		<div style="display: flex; justify-content: space-between; margin: 4px 0; font-size: 16px;"><span>*zzgl. 0% Umsatzsteuer</span><span></span></div>
+		<div style="display: flex; justify-content: space-between; margin: 4px 0; font-size: 10px;"><span>TVA</span><span></span></div>
+		<hr style="border: none; height: 2px; background: #000; margin: 14px 0;">
+		<div style="display: flex; justify-content: space-between; margin: 7px 0; font-size: 16px;"><span><strong>Gesamtbetrag</strong></span><span></span></div>
+		<div style="display: flex; justify-content: space-between; margin: 7px 0;"><span style="font-size: 16px;"><strong>Total</strong></span><span style="font-size: 19px;"><strong>'.number_format($price, 0).' '.$currency.'</strong></span></div>
 	</div>
 	
 	<div>
-		<div style="margin: 12px 0; font-size: 17px;"><strong>*Exportgeschäft . Steuerfreie Ausfurlieferung Paragraf 4 Nr. 1a Ustg</strong></div>
-		<div style="margin: 8px 0; font-size: 12px;"><em>Clauza de livrare fara taxa</em></div>
-		<div style="margin: 12px 0; font-size: 17px;"><strong>Zahlunsart:</strong> Bar/TRANSFER</div>
-		<div style="margin: 8px 0; font-size: 12px;"><em>tip de plata: TRANSFER</em></div>
-		<div style="margin: 12px 0; font-size: 17px;">Das Leistungsdatum entspricht dem Rechnungsdatum.</div>
-		<div style="margin: 8px 0; font-size: 12px;"><em>Data prestației este data facturii.</em></div>
+		<div style="margin: 11px 0; font-size: 16px;"><strong>*Exportgeschäft . Steuerfreie Ausfurlieferung Paragraf 4 Nr. 1a Ustg</strong></div>
+		<div style="margin: 7px 0; font-size: 11px;"><em>Clauza de livrare fara taxa</em></div>
+		<div style="margin: 11px 0; font-size: 16px;"><strong>Zahlunsart:</strong> Bar/TRANSFER</div>
+		<div style="margin: 7px 0; font-size: 11px;"><em>tip de plata: TRANSFER</em></div>
+		<div style="margin: 11px 0; font-size: 16px;">Das Leistungsdatum entspricht dem Rechnungsdatum.</div>
+		<div style="margin: 7px 0; font-size: 11px;"><em>Data prestației este data facturii.</em></div>
 	</div>
 	
-	<div style="margin-top: 10px; padding: 0px; border-top: 2px solid #000;">
+	<div style="margin-top: 9px; padding: 0px; border-top: 2px solid #000;">
 		<div style="display: flex; justify-content: space-between; align-items: flex-start;">
 			<div style="font-size: 16px; flex: 1;">
 				'.str_replace('<br>', '<br>', $seller_address).'
