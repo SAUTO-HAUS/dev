5acc3bd7840628a30ee9d79ba6e5c10ad35cda50
GrigoreBotnarenco <botnarenco1996@mail.com>
2026-02-23 18:53:06 +0200

    update

--- sql_scripts/add_is_at_client_field.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE gh3sp_car_ctlg ADD COLUMN is_at_client TINYINT(1) NOT NULL DEFAULT 0 COMMENT 'Car is with client (excluded from exports)';
-
-ALTER TABLE gh3sp_car_ctlg ADD INDEX idx_is_at_client (is_at_client);
