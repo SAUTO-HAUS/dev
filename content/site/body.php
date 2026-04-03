7d3b43aba3abf9a06de00e0ffc751db93115d649
GrigoreBotnarenco <botnarenco1996@mail.com>
2026-04-03 22:40:17 +0300

    update

--- content/site/body.php
+++ content/site/body.php
@@ -226,28 +226,6 @@ echo '
     </div>
 </header>
 
-<?php
-$_show_links_pages = ['', 'cars', 'ordercars', 'credit', 'services'];
-$_cur_page = isset($t_mp[2]) ? $t_mp[2] : '';
-$_on_services_main = ($_cur_page == 'services' && (!isset($t_mp[3]) || $t_mp[3] == ''));
-if ( in_array($_cur_page, $_show_links_pages) && ($_cur_page != 'services' || $_on_services_main) ):
-?>
-<div id="nav_links">
-    <a href="/<?php echo $_COOKIE['lang']; ?>/calculator">
-        <div class="img" style="background-image:url(/media/images/links/links-1.jpg);"></div>
-        <span class="ttl"><?php echo $lng['p']['services']['calc_customs']['name']; ?></span>
-    </a>
-    <a href="/<?php echo $_COOKIE['lang']; ?>/tradein">
-        <div class="img" style="background-image:url(/media/images/links/links-2.jpg);"></div>
-        <span class="ttl"><?php echo $lng['p']['services']['tradein']['name']; ?></span>
-    </a>
-    <a href="/<?php echo $_COOKIE['lang']; ?>/services/order">
-        <div class="img" style="background-image:url(/media/images/links/links-3.jpg);"></div>
-        <span class="ttl"><?php echo $lng['p']['services']['order']['name']; ?></span>
-    </a>
-</div>
-<?php endif; ?>
-
 <div id="crumbs" data-lng-c="<?php echo $lng['w']['copied']; ?>">
     <?php
     if ( isset($t_mp[2])&&$t_mp[2]!='' ){
@@ -272,6 +250,28 @@ if ( in_array($_cur_page, $_show_links_pages) && ($_cur_page != 'services' || $_
     }
     ?>
 </div>
+
+<?php
+$_show_links_pages = ['', 'cars', 'ordercars', 'credit', 'services'];
+$_cur_page = isset($t_mp[2]) ? $t_mp[2] : '';
+$_on_services_main = ($_cur_page == 'services' && (!isset($t_mp[3]) || $t_mp[3] == ''));
+if ( in_array($_cur_page, $_show_links_pages) && ($_cur_page != 'services' || $_on_services_main) ):
+?>
+<div id="nav_links">
+    <a href="/<?php echo $_COOKIE['lang']; ?>/calculator">
+        <div class="img" style="background-image:url(/media/images/links/links-1.jpg);"></div>
+        <span class="ttl"><?php echo $lng['p']['services']['calc_customs']['name']; ?></span>
+    </a>
+    <a href="/<?php echo $_COOKIE['lang']; ?>/tradein">
+        <div class="img" style="background-image:url(/media/images/links/links-2.jpg);"></div>
+        <span class="ttl"><?php echo $lng['p']['services']['tradein']['name']; ?></span>
+    </a>
+    <a href="/<?php echo $_COOKIE['lang']; ?>/services/order">
+        <div class="img" style="background-image:url(/media/images/links/links-3.jpg);"></div>
+        <span class="ttl"><?php echo $lng['p']['services']['order']['name']; ?></span>
+    </a>
+</div>
+<?php endif; ?>
 
 <?php
 if ( !isset($t_mp[2]) || $t_mp[2]==''){
