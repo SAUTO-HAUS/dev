6da3ff6e698ad036ec525fcaa9ccfc5d9229d836
GrigoreBotnarenco <botnarenco1996@mail.com>
2026-04-04 19:13:04 +0300

    update

--- content/site/body.php
+++ content/site/body.php
@@ -252,7 +252,7 @@ echo '
 </div>
 
 <?php
-$_show_links_pages = ['', 'cars', 'ordercars', 'services'];
+$_show_links_pages = ['', 'cars', 'ordercars', 'services', 'calculator'];
 $_cur_page = isset($t_mp[2]) ? $t_mp[2] : '';
 $_on_services_main = ($_cur_page == 'services' && (!isset($t_mp[3]) || $t_mp[3] == ''));
 if ( in_array($_cur_page, $_show_links_pages) && ($_cur_page != 'services' || $_on_services_main) ):
