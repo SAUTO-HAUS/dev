e0b0ae9cc2ea0a6fb76bd86b56fcb20faf575f91
GrigoreBotnarenco <botnarenco1996@mail.com>
2026-04-07 16:52:51 +0300

    update docs

--- content/admin/include/docs/cars/con_plata.php
+++ content/admin/include/docs/cars/con_plata.php
@@ -17,7 +17,7 @@ $rtrn = '
 <div id="p_cont" class="base">';
 $br = isset($_POST['br']) ? (is_array($_POST['br']) ? ($_POST['br'][0] ?? '') : $_POST['br']) : '';
 $mo = isset($_POST['mo']) ? (is_array($_POST['mo']) ? ($_POST['mo'][0] ?? '') : $_POST['mo']) : '';
-// $vin = isset($_POST['vin']) ? (is_array($_POST['vin']) ? ($_POST['vin'][0] ?? '') : $_POST['vin']) : '';
+$vin = isset($_POST['vin']) ? (is_array($_POST['vin']) ? ($_POST['vin'][0] ?? '') : $_POST['vin']) : '';
 	$sum = 0;
 	for ($i=1;$i<=1;$i++){
 		$sum += (isset($_POST['prc']) && is_numeric($_POST['prc'])) ? floatval($_POST['prc']) : 0;
@@ -32,7 +32,7 @@ $mo = isset($_POST['mo']) ? (is_array($_POST['mo']) ? ($_POST['mo'][0] ?? '') : 
 			<div class="flx">
 				<table>
 					<tr><td class="id">№</td><td class="nm txt_up">Denumirea marfuri (serviciilor)<br/>Название товара (услуг)</td><td class="prc">Pret pentru o unitate<br/>Цена за единицу<br/>'.(isset($_POST['cur'])?$_POST['cur']:'MDL').'</td></tr>
-					<tr><td class="id">1</td><td class="nm txt_up"><span>Plata in avans pentru automobilul </span>'.($br !== '' ? ucwords(strtolower(str_replace('_', ' ', $br))) : '').' '.($mo !== '' ? ucwords(str_replace('_', ' ', $mo)) : '')./* ($vin !== ''?'<br/>VIN: '.$vin:'') */'</td><td class="prc">'.parseCurr($_POST['prc']).'.00</td></tr>
+					<tr><td class="id">1</td><td class="nm txt_up"><span>Plata in avans pentru automobilul </span>'.($br !== '' ? ucwords(strtolower(str_replace('_', ' ', $br))) : '').' '.($mo !== '' ? ucwords(str_replace('_', ' ', $mo)) : '').($vin !== ''?'<br/>VIN: '.$vin:'').'</td><td class="prc">'.parseCurr($_POST['prc']).'.00</td></tr>
 					<tr><td class="id"></td><td class="nm txt_up">TOTAL</td><td class="prc">'.parseCurr($sum).'.00</td></tr>
 				</table>
 				<div class="buyer">Platitor: <span class="txt_cpt">'.(isset($_POST['u_nm']) ? strtolower((string)$_POST['u_nm']) : '').'</span>, '.(isset($_POST['u_tp'])&&$_POST['u_tp']=='fiz'?'cp':'cf').' <span class="txt_up">'.(isset($_POST['u_cf_idno'])?$_POST['u_cf_idno']:'').'</span></div>
